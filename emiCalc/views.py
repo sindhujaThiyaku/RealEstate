e80d265ccca1330da47590f589f5e83131c55347
sindhuja thiyagarajan <sindhuja.thiyagarajan@nexttechnosolutions.co.in@examplenet.xyz>
2018-10-08 18:25:22 +0530

    Changes

--- emiCalc/views.py
+++ emiCalc/views.py
@@ -1,29 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
-import smtplib
-from django.conf import settings
 import redis
-import ast
-from django.contrib.auth.models import User 
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-import json
-import requests
 import math
+import dateutil.parser
 # Create your views here.
 from rest_framework.renderers import TemplateHTMLRenderer
-from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import mixins
 from rest_framework.decorators import api_view, permission_classes, renderer_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework.authentication import TokenAuthentication
 
-from django.utils.crypto import get_random_string
 redis_con = redis.StrictRedis(host="localhost", port="6379", db="0")
 
 
@@ -50,9 +37,10 @@ def emiCalculate(request):
         monthDict = {}
         loanDict = {}
         if principal and rate and duration and fromemi and durationType:
-            print "fromemi",fromemi
-            startMonth = fromemi.split('-')[1]
-            startYear =  fromemi.split('-')[0] 
+            fromemidate = dateutil.parser.parse(fromemi, ignoretz=True).strftime("%m-%Y")
+            print fromemidate
+            startMonth = fromemidate.split('-')[0]
+            startYear =  fromemidate.split('-')[1] 
             principal = float(principal)
             rate = float(rate)
             duration = float(duration)
